31a6eff4b4dce54c5ebc2b945adf6b37872c3710
FernandinOficial <fernandoribeirooficial123@gmail.com>
2024-05-21 17:04:01 -0300

    commit com sql feito

--- formulario.php
+++ formulario.php
@@ -1,19 +1,92 @@
+<?php
+
+    $obj_mysql = mysqli_connect("127.0.0.1","root","","db_supermercado");
+    if (!$obj_mysql) {
+        echo 'Erro ao conectar ao Banco de Dados';
+        exit;
+    }
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário</title>
+    <Style>
+        th{
+            padding: 3px;
+            font-size: 20px;
+            font-family: Arial, Helvetica;
+        }
+        td{
+            padding: 5px;
+            font-size: 18px;
+            margin-left: auto;
+            margin-right: auto;
+            font-family: Arial, Helvetica;
+        }
+        h2{
+            display:flex;
+            justify-content: center;
+            align-items: center;
+            font-family: Arial, Helvetica, bold;
+        }
+    </Style>
 </head>
 <body>
-    <h1>Tabela de Produtos</h1>
+    <h1>Inserir Produtos</h1>
 
-    <form action=""
+    <form action="<?=$_SERVER["PHP_SELF"]?>" method="post">
+      
+        <label for="nome_prod">Nome do Produto:</label><br>
+        <input type="text" maxlength="30" name="nome_prod" id="nome_prod"><br><br>
+        
+        <label for="desc_prod">Descrição do Produto:</label><br>
+        <input type="text" maxlength="50" name="desc_prod" id="desc_prod"><br><br>
 
+        <label for="peso_prod">Unidade do Produto:</label><br>
+        <input type="text" maxlength="3" name="peso_prod" id="peso_prod"><br><br>
 
+        <label for="data_prod">Data de entrega:</label><br>
+        <input type="date" name="data_prod" id="data_prod"><br><br>
+        
+        <label for="val_prod">Valor do Produto (unitário):</label><br>
+        <input type="number" step="0000000000.001" name="val_prod" id="val_prod"><br><br>
 
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Limpar">
+    </form>
+    <br>
+    <br>
+    <table width="55%" border="1" cellspacing="0">
+        <tr>
+        <div style="width: 50%;">    
+            <h2>Tabela Produto</h2>
+        </div>
+            <th><strong>ID</strong></th>
+            <th><strong>Nome</strong></th>
+            <th><strong>Descrição</strong></th>
+            <th><strong>Unidade</strong></th>
+            <th><strong>Data de entrega</strong></th>
+            <th><strong>Valor</strong></th>
+        </tr>
+        
+    <?php  
 
-
-    
+    $resultado = $obj_mysql -> query("SELECT * FROM `produtos`");
+    while ($aux_produtos = $resultado -> fetch_assoc())
+    {
+        echo '<tr>';
+        echo '<td>'.$aux_produtos["id_prod"].'</td>';
+        echo '<td>'.$aux_produtos["nome_prod"].'</td>';
+        echo '<td>'.$aux_produtos["desc_prod"].'</td>';
+        echo '<td>'.$aux_produtos["peso_prod"].'</td>';
+        echo '<td>'.$aux_produtos["data_prod"].'</td>';
+        echo '<td>'.$aux_produtos["val_prod"].'</td>';
+        echo '</tr>';
+    }   
+    ?>
+    </table>
 </body>
 </html>
--- itensProdutosQUERY.sql
+++ itensProdutosQUERY.sql
@@ -4,12 +4,16 @@
 USE db_supermercado;
 
 -- CREATE TABLE produtos (
---     id_prod INT PRIMARY KEY AUTO_INCREMENT,
+--     id_prod INT(10) PRIMARY KEY AUTO_INCREMENT,
 --     nome_prod VARCHAR(30) NOT NULL,
 --     desc_prod VARCHAR(50) NOT NULL,
---     peso_prod SMALLINT(3) NOT NULL,  
+--     peso_prod CHAR(3) NOT NULL,  
 --     data_prod DATE DEFAULT NULL,
 --     val_prod DECIMAL(10,3) NOT NULL
 -- );
 
-SELECT * FROM produtos;
+-- SELECT * FROM produtos;
+
+/*INSERT INTO produtos (nome_prod, desc_prod, peso_prod, data_prod, val_prod) VALUES ('Papel Higienico', 'Esmeralda papel higienico SuperFofo', 'kg', '2023-11-10', 12.5);*/
+
+INSERT INTO produtos (nome_prod, desc_prod, peso_prod, data_prod, val_prod) VALUES ('Gilete master cut', 'Esmeralda papel higienico SuperFofo', 'kg', '2023-11-10', 12.5);
